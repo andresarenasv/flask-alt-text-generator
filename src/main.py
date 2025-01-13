083975a1f393827d1bf0af0a1c16bc6152b2cae5
Andres Arenas <andres.arenas.vargas@gmail.com>
2025-01-12 20:40:29 -0500

    feat: adding modules

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from src.modules.number.routes import number_bp
 from src.modules.image.routes import image_bp
 from src.modules.replicate.routes import replicate_bp
 
-def create_app():
-    app = Flask(__name__)
-        
-    # Register blueprints
-    app.register_blueprint(number_bp)
-    app.register_blueprint(image_bp)
-    app.register_blueprint(replicate_bp)
 
-    return app
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    app = create_app()
+# Register blueprints
+app.register_blueprint(number_bp)
+app.register_blueprint(image_bp)
+app.register_blueprint(replicate_bp)
+
+@app.route("/")
+def index():
+    return "This is an alt tag generator!"
+
+@app.route('/health')
+def health_check():
+    return {'status': 'ok'}
